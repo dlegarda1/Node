ee388421eb2bfc05375c60f499df03f0637e3470
dlegarda <legarda8027@gmail.com>
2024-03-09 22:25:26 -0500

    cambios

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const {sumar}=require("./funciones.js");
-
-console.log(sumar(10,20));
+console.log('Hello World!');
+console.warn('ocurrio un error...');
+console.assert(1 === 2, 'uno no es igual a dos');
+console.table([1, 2, 3, 4, 5]);
+console.error(new Error('ocurrio un error...'));
+//para mostrar imagenes en consola se utiliza el comando anterior y la ruta de la imagen
+console.log('%c Este es un texto con estilo', 'color: red; font-size: 20px');
