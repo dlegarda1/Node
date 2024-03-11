a12b9b3110401bd5d2b3bc4f5641ea3a82f25f90
dlegarda <legarda8027@gmail.com>
2024-03-11 00:59:44 -0500

    ejemplos manejo archivos

--- app.js
+++ app.js
@@ -1,8 +1,55 @@
-const funciones=require('./funciones.js');
+const fs=require('fs');
 
-setTimeout(funciones.sumar, 2000, 5, 3);
-console.log("antes de setImmediate");
-setImmediate(funciones.sumar, 10, 3);
-console.log("despues de setImmediate");
-setInterval(funciones.sumar, 2000, funciones.sumar(funciones.sumar(2,5),5), 3);
-// setTimeout y setInterval ejecutan una función después de un tiempo determinado (en milisegundos) desde que
+console.log("Inicio del programa");
+
+//Lectura de un archivo
+
+const archivo=fs.readFileSync('index.html', 'utf8'); 
+console.log(archivo);
+fs.renameSync("index.html", "main.html");
+fs.writeFileSync("copia.txt","Este es un archivo de copia");
+fs.appendFileSync("copia.txt"," con contenido adicional");
+fs.unlinkSync("copia.txt");
+
+
+
+/*fs.readFile('index.html', 'utf8', (err, contenido) => {
+  if (err){
+    throw err;
+  }else{
+    console.log(contenido);
+    }
+    console.log("Lectura de archivo finalizada");
+});
+/*
+console.log("despues de leer archivo");
+//Renombrar un archivo
+fs.rename("index.html", "main.html", (err) => {
+    if (err){
+        throw err;
+    }console.log("Archivo renombrado");    
+    });
+  
+console.log("despues de renombrar archivo");
+//Agregar contenido a un archivo 
+fs.appendFile("index.html",'<p>Hola Mundo</p>', (err) => {
+    if (err){
+        throw err;
+    }console.log("Archivo modificado");
+    });
+
+    console.log("despues de agregar contenido");
+//Crear un archivo
+fs.writeFile("texto.txt", "Hola Mundo", (err) => {
+        if (err){
+            throw err;
+        }console.log("Archivo creado");
+        });
+console.log("despues de crear archivo");
+//Eliminar un fichero o carpeta
+fs.unlink("texto.txt", (err) => {
+    if (err) throw err;
+    console.log("El archivo se ha eliminado");
+});
+console.log("despues de eliminar archivo");
+*/
